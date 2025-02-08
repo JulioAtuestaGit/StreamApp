91e0a74b8c576298cb1fd7d81d6fdd117f18f428
JulioAtuestaGit <julioatuestagit@gmail.com>
2025-02-07 23:05:03 -0500

    find  item by id

--- src/main/java/com/example/demo/connection/UserCrud.java
+++ src/main/java/com/example/demo/connection/UserCrud.java
@@ -1,40 +1,98 @@
 package com.example.demo.connection;
 import com.example.demo.data.ITableCrud;
 import com.example.demo.model.UserTableMap;
-
 import java.sql.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.demo.connection.dataBaseConnection.dbConection;
 
 /*this runs the CRUD tasks - representen in the interface ItableCrud- for objects type user - mapping the users table -
 * thus we have a calss user to mapp the table, an interface to declare CRUD methods and a class to define those methods
 * applying them to the users , and future classes to apply same methods to productions */
-public class UserCrud implements ITableCrud {
+public class UserCrud implements ITableCrud<UserTableMap> {
 
     @Override
     public List<UserTableMap> listItem() {
         List<UserTableMap> usersList= new ArrayList();
         // here sql statements to retrieve all users in the table showing.. what?
+        PreparedStatement ppst;
+        ResultSet rs;
+        Connection connection = dbConection(); // same as databaseConecction.dcconection() it returns and obj type 'connection'
+        String sqlQuery = "select * from users order by id";
+        try{
+            ppst = connection.prepareStatement(sqlQuery);
+            rs = ppst.executeQuery();
+            while (rs.next()){
+                UserTableMap user = new UserTableMap();
+                user.setId(rs.getInt("id"));
+                user.setUserName (rs.getString("user_name"));
+                user.setUserPassword(rs.getString("user_password"));
+                user.setUserNickname(rs.getString("user_nickname"));
+                usersList.add(user);
+            }
+        }
+        catch (Exception e){
+            System.out.println("error "+e.getMessage());
+        }finally {// se ejecuta siempre incluso si estuviera despues del return
+            try {
+                connection.close();
+            }catch (Exception e){
+                System.out.println("Error closing connection" + e.getMessage());
+            }
+        }
         return usersList;
     }
 
     @Override
-    public Object findId(int id) {
-        return null;
+    public UserTableMap findId(int id) {
+        UserTableMap returnedObj = new UserTableMap();
+        PreparedStatement ppst;
+        ResultSet rs;
+        Connection connection = dbConection();
+        String sqlQuery = "select * from users where id =?"; //? sintaxis query
+        try{
+            ppst = connection.prepareStatement(sqlQuery);
+            ppst.setInt(1, id);
+            rs = ppst.executeQuery();
+            if(rs.next()){
+                returnedObj.setId(id);
+                returnedObj.setUserName(rs.getString("user_name"));
+                returnedObj.setUserNickname(rs.getString("user_nickname"));
+                returnedObj.setUserPassword(rs.getString("user_password"));
+            }
+        }catch (Exception e){
+            System.out.println("Error metodo FindID en UserCrud "+e.getMessage());
+        }finally {
+            try {
+                connection.close();
+            }catch (Exception e){
+                System.out.println("Error closing connection" + e.getMessage());
+            }
+        }
+
+        return returnedObj;
     }
 
     @Override
-    public boolean addItem(Object item) {
-        return false;
+    public boolean addItem(UserTableMap item) {
+        return true;
     }
 
     @Override
-    public boolean modifyItem(Object item) {
+    public boolean modifyItem(UserTableMap item) {
         return false;
     }
 
     @Override
     public boolean deleteItem(int id) {
         return false;
+    }
+    public void main(){
+        System.out.println(listItem());
+        System.out.println(findId(3));
     }
 }
--- src/main/java/com/example/demo/model/UserTableMap.java
+++ src/main/java/com/example/demo/model/UserTableMap.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class UserTableMap {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     @Column(name = "user_name")
     private String userName;
     @Column(name = "user_password")
@@ -20,15 +20,35 @@ public class UserTableMap {
     @Column(name = "user_history")
     private String userHistory;
 */
+
+    public UserTableMap() {
+    }
+
     public UserTableMap(String userName, String userPassword, String userNickname) {
         this.userName = userName;
         this.userPassword = userPassword;
         this.userNickname = userNickname;
     }
 
-    public Long getId() { return id; }
+    @Override
+    public String toString() {
+  /*      StringBuilder stringBuilder = new StringBuilder();
+        usersList.forEach(user ->{
+            stringBuilder.append(user.getId() + " " +user.getUserName() +" "+ user.getUserNickname()+"\n");
+        });
+        System.out.println(stringBuilder);*/
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(this.getId()+" "+this.getUserName() +" "+this.getUserPassword()+" "+this.getUserNickname()+"\n");
+        return stringBuilder.toString();
+    }
+
+    public int getId() { return id; }
     public String getUserName() { return userName; }
-    public void setUserName(String userName) { this.userName = userName; }
     public String getUserPassword() { return userPassword; }
+    public String getUserNickname() {return userNickname;}
+
+    public void setId(int id) {this.id = id;}
+    public void setUserName(String userName) { this.userName = userName; }
     public void setUserPassword(String userPassword) { this.userPassword = userPassword; }
+    public void setUserNickname(String userNickname) {this.userNickname = userNickname;}
 }
