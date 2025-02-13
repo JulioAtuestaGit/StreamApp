fbd0dcce39e12e700c5699bf6a14cbf090ebc698
JulioAtuestaGit <julioatuestagit@gmail.com>
2025-02-13 00:05:55 -0500

    methods syntax corrected//delete and find by email added

--- src/main/java/com/example/demo/services/UserServiceRepo.java
+++ src/main/java/com/example/demo/services/UserServiceRepo.java
@@ -2,11 +2,9 @@ package com.example.demo.services;
 import com.example.demo.Repository.CrudRepository;
 import com.example.demo.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
+/*Why do I need this class if we are only calling crud repository methods with no other logic*/
 @Service
 public class UserServiceRepo implements IUserService{
 
@@ -15,24 +13,27 @@ public class UserServiceRepo implements IUserService{
 
     @Override
     public List<Users> usersList() {
-        List<Users> usersList = crudRepository.findAll();
-        return List.of();
+        return crudRepository.findAll();
     }
 
     @Override
     public Users getUserById(Integer id) {
-        Users returnedUser = crudRepository.findById(id).orElse(null);// find by id returns an optional value
-        return returnedUser;
+        return crudRepository.findById(id).orElse(null);// find by id returns an optional value
     }
 
     @Override
     public void savedUser(Users user) {
         crudRepository.save(user);
-
     }
 
     @Override
     public void deleteUSer(Users user) {
         crudRepository.delete(user);
+    }
+
+    @Override
+    public boolean findByUserEmail(String userEmail) {
+
+        return false;
     }
 }
